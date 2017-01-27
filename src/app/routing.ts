858095e66f66a2299ab6ced6538c2c8f28ab412f
quinnschwartz <quinnschwartz@gmail.com>
2017-01-27 13:41:18 -0800

    set up router.

---
+++ src/app/routing.ts
@@ -0,0 +1,32 @@
+import { ModuleWithProviders }  from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { MusiciansComponent }   from './musicians/musicians.component';
+import { MusicianDetailComponent }   from './musician-detail/musician-detail.component';
+import { AdminComponent }   from './admin/admin.component';
+
+const appRoutes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  {
+    path: 'musicians',
+    component: MusiciansComponent
+  },
+  {
+    path: 'musician/:id',
+    component: MusicianDetailComponent
+  },
+  {
+    path: 'admin',
+    component: AdminComponent
+  }
+];
+
+export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
